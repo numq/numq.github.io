a8875802984f3184d82bcb5ab1f7132e334bd4f2
numq <numq.dev@gmail.com>
2021-06-08 21:05:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var doomerAudio = document.getElementById("doomerAudio");
+var bloomerAudio = document.getElementById("bloomerAudio");
 
+function playDoomer() {
+  bloomerAudio.pause();
+  doomerAudio.play();
+}
+
+function playBloomer() {
+  bloomerAudio.play();
+  doomerAudio.pause();
+}
