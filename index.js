c76b3dcfe9f8abbf18fff1b0fdb75178ca36403d
numq <numq.dev@gmail.com>
2021-06-08 21:20:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var doomerAudio = document.getElementById("doomerAudio");
 var bloomerAudio = document.getElementById("bloomerAudio");
 
+doomerAudio.volume = 0.5;
+doomerAudio.volume = 0.5;
+
 function playDoomer() {
   bloomerAudio.pause();
   doomerAudio.play();
